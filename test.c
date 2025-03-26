d5db21844281cd01ee7b1595b9f26358c32302af
Guillaume <guifroment0@gmail.com>
2025-03-26 09:22:16 +0100

    test commit2

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#define WIDTH 800
+#define Height 600
+
+int main( int argc, char argv[])
+{
+    SDL_init(SDL_INIT_EVERYTHING);
+    //SDL_Window * window = SDL_CreateWindow("Hello world",SDL_WINDOWPOS_UNDEFINED,0,500,500, SDL_WINDOW_SHOWN);
+    SDL_Delay(1000);
+
+}
